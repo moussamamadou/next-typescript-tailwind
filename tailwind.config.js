f812227f00680d7fb97483135ee2b690d93494ab
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-13 00:27:11 +0200

    Update Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { colors } = require(`tailwindcss/defaultTheme`);
 
 module.exports = {
   mode: "jit",
-  purge: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   experimental: {
     applyComplexClasses: true,
   },
